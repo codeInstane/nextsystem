f259af72b674fb1257332b7b25e0ab5ee7e5b760
codeInstane <fabian.savill.2020@gmail.com>
2023-05-28 17:41:57 +0800

    can display angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,30 @@ import { RouterModule, Routes } from '@angular/router';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* { path: '', component: AppComponent }, */
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'nav', component: NavigationComponent },
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {
+export const appRouting = RouterModule.forRoot(routes);
 
-  constructor(route: ActivatedRoute) {
-    // const id: Observable<string> = route.params.pipe(map(p => p.id));
-    // const url: Observable<string> = route.url.pipe(map(segments => segments.join('')));
-    // // route.data includes both `data` and `resolve`
-    // const user = route.data.pipe(map(d => d.user));
-  }
- }
+
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {
+
+//   constructor(route: ActivatedRoute) {
+//     // const id: Observable<string> = route.params.pipe(map(p => p.id));
+//     // const url: Observable<string> = route.url.pipe(map(segments => segments.join('')));
+//     // // route.data includes both `data` and `resolve`
+//     // const user = route.data.pipe(map(d => d.user));
+//   }
+//  }
